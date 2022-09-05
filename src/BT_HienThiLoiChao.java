039570a1f4f27261bcb815697b25714bf6b74b01
CoraSan1 <nguyendinh070199@gmail.com>
2022-09-05 17:19:42 +0700

    Init

---
+++ src/BT_HienThiLoiChao.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BT_HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ten cua ban ");
+        String a = " ";
+        a = sc.nextLine();
+        System.out.println("Hello " + a);
+    }
+}
