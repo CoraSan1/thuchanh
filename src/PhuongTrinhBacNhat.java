bac7a6e1532b3488eef8845d57706f14c68388d7
CoraSan1 <nguyendinh070199@gmail.com>
2022-09-05 15:21:57 +0700

    Init

---
+++ src/PhuongTrinhBacNhat.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PhuongTrinhBacNhat {
+    public static void main(String[] args) {
+        System.out.println("Cho phuong trinh ax + b = c ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap so a ");
+        double a = sc.nextDouble();
+        System.out.print("nhap so b ");
+        double b = sc.nextDouble();
+        System.out.print("nhap so c ");
+        double c = sc.nextDouble();
+        if (a != 0){
+            double answer = (c - b)/a;
+            System.out.println(answer);
+        }
+        else {
+            if (b == c){
+                System.out.println("Phuong trinh vo so nghiem");
+            }
+            else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        }
+    }
+}
